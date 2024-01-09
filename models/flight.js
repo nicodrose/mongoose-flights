331954c7df6ecbe6b0e07d112f29504b1b99a74c
nicodrose <ndesro@gmail.com>
2024-01-08 16:32:23 -0800

    Includes default: 'SEA'for airport flightSchema

--- models/flight.js
+++ models/flight.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
     airline: { type: String, enum: ['Alaska', 'Delta', 'American', 'United'], require: true }, 
-    airport: { type: String, enum: ['SEA', 'DFW', 'DEN', 'LAX', 'JFK', 'SFO'], required: true },
+    airport: { type: String, default: 'SEA', enum: ['SEA', 'DFW', 'DEN', 'LAX', 'JFK', 'SFO'], required: true },
     flightNo: { type: Number, min: 10, max: 9999, required: true },
     departs: { 
         type: String, default: function () {
